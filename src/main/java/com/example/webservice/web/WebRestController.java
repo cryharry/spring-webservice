52ab642e12b9cf8bd6df4c3e54155fcec9cbf1ef
cryharry <cryharryk@gmail.com>
2018-11-28 14:09:53 +0900

    ecs 배포전 커밋

--- src/main/java/com/example/webservice/web/WebRestController.java
+++ src/main/java/com/example/webservice/web/WebRestController.java
@@ -1,12 +1,27 @@
 package com.example.webservice.web;
 
+import com.example.webservice.dto.PostsSaveRequestDto;
+import com.example.webservice.service.PostsService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsService postsService;
+
     @GetMapping("/hello")
     public String hello() {
         return "Hello World!";
+    }
+
+    @PostMapping("/posts")
+    public Long savePosts(@RequestBody PostsSaveRequestDto dto) {
+        return postsService.save(dto);
     }
 }
